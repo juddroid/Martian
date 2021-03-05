fb34483910b9e31679dca9ae295dbb1af27296fd
juddroid <70361152+juddroid@users.noreply.github.com>
2021-03-06 06:56:13 +0900

    [Raccoon] martian day4 (#67)
    * [Martian]
    
    * [Martian] dec hex test
    
    * [Martian] hex2str str2hex
    
    * [Martian] 반나절만에 조각내기 성공
    
    * [Martian] 조각에 숫자를 넣었다.
    
    * [Earth] 지구에서 입력준비 완료
    
    * [Mars] 화성에서도 실시간 해석기능 추가
    
    * [Mars] RaccoonPromise 구현 준비
    
    * [Mars] RaccoonPromise Contructor 추가
    
    * [Martian] 지구 -> 화성 동기로 작동하는 단방향 통신
    
    * [Martian] 오타수정 / 이런 오타가 숨어있었는데 정상 작동했었다니...
    
    * [Martian] 화살표 애니메이션하다가 자러감
    
    * [리뷰반영] PIECE 삭제
    
    * [Mars] 화살표 애니메이션 구현 / forEach의 작동방식을 제대로 몰라서 setTimeOut으로 interval을 만들려고 했던게 어려움이 컸다. 결국 promise, async, await 다 쓰고, forEach도 for 혹은 for of로 바꿔야 했다. 그 와중에 parcel, babel 관련 오류가 있어서 더 힘든 하루 :)
    
    * [Mars] 화살표 애니메이션 후 입력값 하나씩 전달
    
    * [Mars] 화살표 애니메이션 수정
    
    * [Mars] 함수 쪼개보기
    
    * 이상하게 됐따... 큰일이군..
    
    * 이상하게 됐따... 큰일이군..
    
    * 돌아갈래 ㅠㅠ
    
    * 된건가
    
    * 또 왜이래...
    
    * 이건 또 뭐지
    
    * 일단 해보자2
    
    * 이상하게 됐따... 큰일이군..
    
    * 일단 해보자3
    
    * 된건가
    
    * 된건가
    
    * 된건가
    
    * 일단 해보자4
    
    * 도대체 무슨일이지
    
    * 도대체 무슨일이지2
    
    * 도대체 무슨일이지3
    
    * 도대체 무슨일이지4
    
    * 도대체 무슨일이지5
    
    * 몰라이제
    
    * 돌아갈 수 있을까 reset commit id로 시도
    
    * rebase를 벗어나지를 못하고 있다
    
    * rebase를 벗어나지를 못하고 있다
    
    * aaa
    
    * 손으로 복구 ㅡㅡ^
    
    * 손으로 복구 ㅡㅡ^2
    
    * 새 브랜치 test
    
    * 새 브랜치 test2
    
    * [Mars] 마음을 다잡고 5초마다 수신 계획중
    
    * [Mars] 메세지 발신 후 2초 후 송신
    
    * [Mars] 5초마다 큐에서 메세지 수거
    
    * [Mars] 기획서 요구조건 중 동작 방식은 어느정도 완료
    * Bug
    	- 데이터가 완전히 수신되기 전에 버튼이 활성화가 됨
    	- 화살표 방향이 가까운 곳으로 돌지 않음
    * UI 로직에 문제가 있다.
    	- 버튼 활성화/비활성화가 직관적이지 않음
    	- 여러개의 큐가 들어올 수 있도록 만들어 놓고,
    		- 띄어쓰기 문제와
    		- 인풋창 리셋 문제를
    		- 해결하지 못해서 로직을 바꿨다.
    * Promise 문제 / myPromise 구현문제
    * 전역에 있는 변수를 함수로 넣기
    * 조금은 더 함수형 답게 수정하기
    * ES Module 사용하지 못하는 문제 해결하기(parcel 오류 수정 or webpack시도)
    
    Co-authored-by: Juddroid <nobllizdc@gamil.com>

--- raccoon-martian-express/martian/receiver.js
+++ raccoon-martian-express/martian/receiver.js
@@ -1,4 +1,3 @@
-
 const $earthInterpretButton = document.querySelector('#earth-interpret--button');
 const $marsInterpretButton = document.querySelector('#mars-interpret--button');
 const $earthInput = document.querySelector('#earth--input');
@@ -7,13 +6,52 @@ const $earthInfo = document.querySelector('#earth--info');
 const $marsInfo = document.querySelector('#mars--info');
 const $send2earthButton = document.querySelector('#send-to-earth--button');
 const $send2marsButton = document.querySelector('#send-to-mars--button');
+const $arrow = document.querySelector('.arrow-circle');
+const $roulette = document.querySelector('.roulette');
+
+const MARS = 'mars';
+const EARTH = 'earth';
+
+const colorSet = {
+  blue: '#0d6efd',
+  indigo: '#6610f2',
+  purple: '#6f42c1',
+  pink: '#d63384',
+  red: '#dc3545',
+  orange: '#fd7e14',
+  yellow: '#ffc107',
+  green: '#198754',
+  teal: '#20c997',
+  cyan: '#0dcaf0',
+  gray: '#6c757d',
+  grayDark: '#343a40',
+  primary: '#0d6efd',
+  secondary: '#6c757d',
+  success: '#198754',
+  info: '#0dcaf0',
+  warning: '#ffc107',
+  danger: '#dc3545',
+  dark: '#212529',
+};
 
 $earthInterpretButton.addEventListener('click', interpretor);
 $marsInterpretButton.addEventListener('click', interpretor);
 $earthInput.addEventListener('keyup', immiInterpretor);
 $marsInput.addEventListener('keyup', immiInterpretor);
+$earthInput.addEventListener('click', initInput);
+$marsInput.addEventListener('click', initInput);
 $send2marsButton.addEventListener('click', send2mars);
 $send2earthButton.addEventListener('click', send2earth);
+
+// Send to Mars 버튼을 누르면 여기에 데이터가 저장된다.
+// 이걸 서버에 저장할 수 있나...
+let hexQueue = [];
+let receiver;
+let arrowStatus = false;
+
+function initInput(e) {
+  if (e.currentTarget.value.length > 0) e.currentTarget.value = '';
+}
 
 function immiInterpretor(e) {
   const isEarthInput = () => e.target.id === 'earth--input';
@@ -25,8 +63,16 @@ function immiInterpretor(e) {
 function interpretor(e) {
   const isEarthInterpretButton = () => e.target.id === 'earth-interpret--button';
   const isMarsInterpretButton = () => e.target.id === 'mars-interpret--button';
-  if (isEarthInterpretButton()) $earthInfo.value = hex2str($earthInfo.value);
-  if (isMarsInterpretButton()) $marsInfo.value = hex2str($marsInfo.value);
+  if (isEarthInterpretButton()) {
+    $earthInfo.value = hex2str($earthInfo.value);
+    $earthInterpretButton.disabled = true;
+    $send2earthButton.disabled = false;
+  }
+  if (isMarsInterpretButton()) {
+    $marsInfo.value = hex2str($marsInfo.value);
+    $marsInterpretButton.disabled = true;
+    $send2marsButton.disabled = false;
+  }
 }
 
 function str2hex(str) {
@@ -43,31 +89,127 @@ function hex2str(hex) {
     .join('');
 }
 
-function sendMessage() {}
+function initDom(dom) {
+  if (arrowStatus) return;
+  if (dom.value.length > 0) dom.value = '';
+}
+
+function setHexQueue() {
+  let queue = [];
+  $earthInfo.value.split('').forEach((el) => queue.push(el));
+  hexQueue.push(queue);
+  return hexQueue;
+}
+
+function isEmpty(arr) {
+  return arr.length === 0;
+}
+
+function messageReceiver() {
+  return new Promise((resolve) => {
+    resolve(hexQueue);
+  }).then((data) => {
+    if (isEmpty(data)) return;
+    moveArrow(EARTH, data);
+  });
+}
+
+function startReceiver() {
+  // console.log('start receiver');
+  if (receiver) stopReceiver();
+  receiver = setInterval(messageReceiver, 5000);
+}
+
+function stopReceiver() {
+  // console.log('stop receiver');
+  clearInterval(receiver);
+}
+
+startReceiver();
 
 function send2mars() {
-  return moveArrow($earthInfo.value);
-}
-function moveArrow(value) {
-  setHexData(value);
-}
-
-function setHexData(value) {
-  $marsInfo.value = value;
+  setHexQueue();
+  $send2marsButton.disabled = true;
+  initDom($marsInfo);
+  return;
 }
 
-function send2earth() {}
+function send2earth() {
+  initDom($earthInfo);
+  $send2earthButton.disabled = true;
+  return moveArrow(MARS, [$marsInfo.value.split('')]);
+}
 
-let helloHex = str2hex('hello');
+function moveArrow(planet, valueArr) {
+  const inputHexOnMars = (str) => ($marsInfo.value += str);
+  const inputHexOnEarth = (str) => ($earthInfo.value += str);
+  const delay = () => new Promise((resolve) => setTimeout(resolve, 1000));
+  const hexCode = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  let piece = 360 / hexCode.length;
 
-console.log(`test hex: ${helloHex}`);
-console.log(`test str: ${hex2str(helloHex)}`);
+  let arr = Array.prototype.slice.call(valueArr[0]); // 배열복사
+
+  hexQueue.shift();
+  console.log(hexQueue);
+  (async () => {
+    for (let el of arr) {
+      arrowStatus = true;
+      await delay(); // 아.. 얘가 '기다려~' 하는 애라서 await이네...
+      // await은 promise를 기다린다. from DD
+      let idx = hexCode.indexOf(el);
+      if (idx === -1) {
+        spaceAnimation();
+      } else {
+        let rotateDeg = (piece * idx * 2) / 2 + 10; // 보정
+        arrowAnimation(rotateDeg);
+        pieceAnamation(idx);
+      }
+
+      // 여긴 못빼고있다.....
+      // ...아...
+      setTimeout(() => {
+        if (planet === EARTH) inputHexOnMars(el);
+        if (planet === MARS) inputHexOnEarth(el);
+      }, 1000);
+    }
+    if (planet === EARTH) $marsInterpretButton.disabled = false;
+    if (planet === MARS) $earthInterpretButton.disabled = false;
+
+    arrowStatus = false;
+  })();
+}
+
+function spaceAnimation() {
+  $arrow.style.background = `${Object.values(colorSet)[Math.floor(Math.random() * (Object.values(colorSet).length - 0) + 0)]}`;
+  $arrow.style.transition = `1s ease-in-out`;
+  setTimeout(() => {
+    $arrow.removeAttribute('style');
+    $arrow.style.transition = `1s ease-in-out`;
+  }, 1000);
+}
+
+function arrowAnimation(deg) {
+  $arrow.style.transform = `rotate(${deg}deg)`;
+  $arrow.style.transition = `1s ease-in-out`;
+}
+
+function pieceAnamation(i) {
+  setTimeout(() => {
+    $roulette.querySelector(`.piece-${i}`).style.borderTopColor = `${Object.values(colorSet)[i]}`;
+    $roulette.querySelector(`.piece-${i}`).style.opacity = `50%`;
+    $roulette.querySelector(`.piece-${i}`).style.transition = `1s ease-in-out`;
+  }, 500);
+  setTimeout(() => {
+    $roulette.querySelector(`.piece-${i}`).removeAttribute('style');
+    $roulette.querySelector(`.piece-${i}`).style.transition = `1s ease-in-out`;
+  }, 1000);
+}
 
 // ===== 여기는 지구 =====
 
-// 1. 지구에서 메세지가 발신된다.
-// 2. 메세지가 작성될 때, INFO에 변환 메세지가 나타난다.
-//  ( 화성에서 화살표가 잘 작동하는지 보기 위해서 )
+// v 1. 지구에서 메세지가 발신된다.
+// v 2. 메세지가 작성될 때, INFO에 변환 메세지가 나타난다.
+//   ( 화성에서 화살표가 잘 작동하는지 보기 위해서 )
 
 // ===== 여기는 화성 =====
 
@@ -77,20 +219,20 @@ console.log(`test str: ${hex2str(helloHex)}`);
 //    2-1. 없으면 다시
 // 3. 큐에 담긴 메세지를 하나씩 화살표에 전달한다.
 //    3-1. 전달된 메세지는 큐에서 삭제
-// 4. 화살표가 2초마다 hex-code를 가리킨다.
-//    4-1. 배경이 깜빡거리는 애니메이션이 발생한다.
-//    4-2. 글씨도 반전된다.
-//    4-3. 화살표에도 애니메이션이 있다.
-//    4-4. 화살표는 가까운 방향으로 회전한다.
-// 5. 화살표가 hex-code를 가리킬 때, INFO에 글자가 나타난다.
+//v 4. 화살표가 2초마다 hex-code를 가리킨다.
+//   v 4-1. 배경이 깜빡거리는 애니메이션이 발생한다.
+//   v 4-2. 글씨도 반전된다.
+//   v 4-3. 화살표에도 애니메이션이 있다.
+//   v 4-4. 화살표는 가까운 방향으로 회전한다.
+// v 5. 화살표가 hex-code를 가리킬 때, INFO에 글자가 나타난다.
 // 6. 송신이 완료되고 (if promise가 5초간 펜딩이면) Interpret 버튼을 활성화시킨다.
-// 7. 버튼을 누르면 해석이 된다.
+// v 7. 버튼을 누르면 해석이 된다.
 
 // <발신모드>
-// 8. input에 메세지가 입력되면 info에 실시간으로 해석된다.
-// 9. 버튼을 누르면 화살표가 2초간격으로 메세지를 가리킨다.
-// 10. 화살표가 메세지를 가리킬때 지구의 INFO에 문자가 바로 출력된다.
-// 11. Interpret하면 해석한다.
+// v 8. input에 메세지가 입력되면 info에 실시간으로 해석된다.
+// v 9. 버튼을 누르면 화살표가 2초간격으로 메세지를 가리킨다.
+// v 10. 화살표가 메세지를 가리킬때 지구의 INFO에 문자가 바로 출력된다.
+// v 11. Interpret하면 해석한다.
 
 class RaccoonPromise {
   constructor() {
