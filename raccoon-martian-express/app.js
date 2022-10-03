6cf67fd750c81c821d4d5a44b1f5dfc4621c6657
Raccoon <nobllizdc@gmail.com>
2022-10-04 00:17:41 +0900

    [feat] cors middleware 추가

--- raccoon-martian-express/app.js
+++ raccoon-martian-express/app.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const cors = require('cors');
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
 
+const indexRouter = require('./routes/index');
+const receiveRouter = require('./routes/receiver');
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/receiver', receiveRouter);
 
 module.exports = app;
